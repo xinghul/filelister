e83e0c20f45a6137943408cbe8826354fe535af9
Levi Lu <llu@xcalar.com>
2015-03-24 17:45:50 -0700

    add basic list as json

--- index.js
+++ index.js
@@ -54,11 +54,38 @@
         return list(this.getAbsolutePath(dir));
     };
 
-    files.prototype.listFilesJson = function(dir, callback) {
-        function list(dir) {
+    files.prototype.list = function(dir, callback) {
+        function walk(dir) {
 
         }
         // var json = list
+    };
+
+    files.prototype.listSync = function(dir) {
+        function walk(dir) {
+            var stats = fs.lstatSync(dir)
+            ,   ret   = {};
+            if (!stats.isDirectory()) {
+                return ret;
+            }
+            var contents = fs.readdirSync(dir)
+            ,   files    = []
+            ,   dirs     = [];
+            for (var i = 0, content = null; i < contents.length; i ++) {
+                content = contents[i];
+                stats = fs.lstatSync(path.join(dir, content));
+                if (stats.isFile()) {
+                    files.push(content);
+                } else if (stats.isDirectory()) {
+                    dirs.push(walk(path.join(dir, content)));
+                }
+            }
+            ret.files = files;
+            ret.dirs  = dirs;
+            ret.path  = dir;
+            return ret;
+        }
+        return walk(this.getAbsolutePath(dir));
     };
 
     files.prototype.getAbsolutePath = function(name) {
